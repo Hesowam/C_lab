b2a47c51a78e7db6de5c86d8af880c143471d187
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2020-06-10 00:27:34 +0300

    Update 8-2(Factoral-recursion).c.c

--- C_lab8/8-2(Factoral-recursion).c.c
+++ C_lab8/8-2(Factoral-recursion).c.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-int Counting(int n) {
-    int c;
-    c = Factorial(n) / (Factorial(2) * Factorial(n - 2));
-}
+
 int Factorial(int n) {
     if (n <= 1) { return 1; }
     else { return (n * Factorial(n - 1)); }
 }
 
-void main() {
+int Counting(int n) {
+    return Factorial(n) / (Factorial(2) * Factorial(n - 2));
+}
+
+int main() {
     printf("%d - straight lines can be drawn through 8 points, of which no three lie on one straight line.\n", Counting(8));
+    return 0;
 }
