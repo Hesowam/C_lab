198fcb18c93040effd4cad053ecc3cb55080b8d9
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2020-06-10 00:28:03 +0300

    Update 8-2(Factoral-recursion).c.c

--- C_lab8/8-2(Factoral-recursion).c.c
+++ C_lab8/8-2(Factoral-recursion).c.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int Factorial(int n) {
-    if (n <= 1) { return 1; }
-    else { return (n * Factorial(n - 1)); }
+    if (n <= 1) return 1;
+    return (n * Factorial(n - 1));
 }
 
 int Counting(int n) {
